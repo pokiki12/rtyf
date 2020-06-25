6b2a6d503ba21ab81a7f2fd05e96c6e69c979ada
pokiki12 <67225242+pokiki12@users.noreply.github.com>
2020-06-25 15:35:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,9 +146,6 @@ message.channel.send('#profile')
     var text = message.content.split(' ').slice(1).join(' ');
      message.channel.send(text);
   }
-  if(message.content === 'cr2'){
-message.channel.send("#credit "+"<@" + myid + ">" + "5000")
-  }
 });
  
 client3.on('message', message => {
